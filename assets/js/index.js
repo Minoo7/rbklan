91e617177c55fa47495f35a79f960a17c3da92ac
Leo2659 <ledia_01@edu.sollentuna.se>
2019-10-10 21:07:54 +0200

    2019-10-10

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,6 +20,7 @@ var x = setInterval(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    $("#live").hide();
 
     // Display the result in the element with id="demo"
     //document.getElementById("countdown").innerHTML = days + "" + hours + " " +
@@ -41,3 +42,29 @@ var x = setInterval(function () {
         $("#live").show();
     }
 }, 1000);
+
+
+//Animation
+window.addEventListener("scroll", function() {showFunction()});
+
+//Första HR
+function showFunction() {
+    /*if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        document.getElementsByTagName("hr")[0].style.display = "block";
+    }
+    else {
+        document.getElementsByTagName("hr")[0].style.display = "none";
+    }*/
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        $("#spel-container *").css("visibility", "inital");
+    }
+    else {
+        $("#spel-container *").css("visibility", "hidden");
+    }
+    /*if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        document.getElementsByTagName("hr")[1].style.display = "block";
+    }
+    else {
+        document.getElementsByTagName("hr")[1].style.display = "none";
+    }*/
+}
