9e6155b9e7bcef3446a24e7a6f017ccb401eb576
Leo2659 <ledia_01@edu.sollentuna.se>
2019-10-10 18:13:05 +0200

    2019-10-10

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -4,7 +4,7 @@ function minTwoDigits(n) {
 
 // Set the date we're counting down to
 //var countDownDate = new Date("Jan 7, 2020 17:00:00").getTime();
-var countDownDate = new Date("Oct 1, 2019 17:33:00").getTime();
+var countDownDate = new Date("Mar 6, 2020 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
