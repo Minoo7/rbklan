e2c6f676b20947297f36ab1b5c57481be884b5bb
minoo7 <accounts@minoo.pro>
2019-11-08 18:35:32 +0100

    2019-11-08

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 "use strict"
 
-$(document).ready(function(){
-	$("#hamburger").click(function (){
-	  $("div.overlay").fadeToggle( "fast", "linear" ) ;
+$(document).ready(function () {
+	$("#hamburger").click(function () {
+		$("div.overlay").fadeToggle("fast", "linear");
 	});
 });
 
 (function () {
 
 	$("#hamburger").click(function () {
+		$("html").toggleClass("ham_on");
 		return $("#hamburger").toggleClass("cross");
 	});
 
 	$(document).scroll(function () {
 		var $nav = $("header");
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-	  });
+	});
 
 }).call(this);
