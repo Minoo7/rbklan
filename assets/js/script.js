ffc2608d328af6088c6ee56ec566766d19896ffe
Leo2659 <ledia_01@edu.sollentuna.se>
2019-11-12 21:37:47 +0100

    2019-11-12

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,25 @@ $(document).ready(function () {
 			$(this).addClass('active'); $(this).parents('li').addClass('active');
 		}
 	});
+
+
+	$(document).scroll(function () {
+		var $nav = $("header");
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+
+	
+
+	
 });
+
+
 
 (function () {
 
 	$("#hamburger").click(function () {
 		$("html").toggleClass("ham_on");
 		return $("#hamburger").toggleClass("cross");
-	});
-
-	$(document).scroll(function () {
-		var $nav = $("header");
-		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 
 }).call(this);
