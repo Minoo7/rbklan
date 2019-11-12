543c84d08ff087f4b5ac9d996c8a20dbcc146393
Minoo7 <accounts@minoo.pro>
2019-11-12 14:13:15 +0100

    d

--- assets/js/index.js
+++ assets/js/index.js
@@ -132,7 +132,8 @@ var x = setInterval(function () {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
     
     //var second_1 = Math.round(minTwoDigits(seconds) /= 10);
 
