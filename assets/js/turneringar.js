63fe9457c63b3d923cc76eeec84de079eb468042
minoo7 <accounts@minoo.pro>
2019-11-17 13:05:12 +0100

    2019-11-17

--- assets/js/turneringar.js
+++ assets/js/turneringar.js
@@ -4,24 +4,27 @@ window.onload = (function () {
     var topMargin = $(".content").position().top;
     var bottomMargin = 58;
 
-    var l_h = $("#lol .content").height()+topMargin+bottomMargin;
-    var c_h = $("#csgo .content").height()+topMargin+bottomMargin;
-    var f_h = $("#fortnite .content").height()+topMargin+bottomMargin;
-    
+    var l_h = $("#lol .content").height() + topMargin + bottomMargin;
+    var c_h = $("#csgo .content").height() + topMargin + bottomMargin;
+    var f_h = $("#fortnite .content").height() + topMargin + bottomMargin;
+
     $("#lol-bg").height(l_h);
     $("#csgo-bg").height(c_h);
     $("#fortnite-bg").height(f_h);
+});
+
+$(document).ready(function () {
     document.getElementsByTagName("html")[0].style.visibility = "visible";
 });
 
-$(window).on('resize', function (){
+$(window).on('resize', function () {
     var topMargin = $(".content").position().top;
     var bottomMargin = 58;
 
-    var l_h = $("#lol .content").height()+topMargin+bottomMargin;
-    var c_h = $("#csgo .content").height()+topMargin+bottomMargin;
-    var f_h = $("#fortnite .content").height()+topMargin+bottomMargin;
-    
+    var l_h = $("#lol .content").height() + topMargin + bottomMargin;
+    var c_h = $("#csgo .content").height() + topMargin + bottomMargin;
+    var f_h = $("#fortnite .content").height() + topMargin + bottomMargin;
+
     $("#lol-bg").height(l_h);
     $("#csgo-bg").height(c_h);
     $("#fortnite-bg").height(f_h);
